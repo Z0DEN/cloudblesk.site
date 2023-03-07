bae8d4be98ed29c3338d0fc621e56b7a48c0d24d
Z0DEN <maksarestov2000@gmail.com>
2023-03-07 23:40:50 +0700

    index-js

---
+++ www/cloudblesk.site/html/test.js
@@ -0,0 +1,24 @@
+"use strict";
+document.querySelector("#submit").onclick = function() {
+    let age = prompt('Сколько тебе лет?', 100);
+
+    alert(`Тебе ${age} лет!`);
+}
+
+function white() {
+    alert("white")
+}
+
+function blue() {
+    alert("blue")
+}
+
+function red() {
+    alert("red")
+}
+
+function age() {
+    let age = prompt('Сколько тебе лет?');
+
+    alert(`Тебе ${age} лет!`);
+}
