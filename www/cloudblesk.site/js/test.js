62306aa425b5e3cda9113bd7a03f74c363b81ab0
Z0DEN <maksarestov2000@gmail.com>
2023-03-09 01:39:37 +0700

    jst-a-chng

---
+++ www/cloudblesk.site/js/test.js
@@ -0,0 +1,28 @@
+function submit() {
+    let age = alert('button is pressed');
+}
+
+function white() {
+    alert("white is pressed")
+}
+
+function blue() {
+    alert("blue is pressed")
+}
+
+function red() {
+    alert("red is pressed")
+}
+
+function age() {
+    let age = prompt('Сколько тебе лет?');
+    alert(`Тебе ${age} лет!`);
+}
+
+function redirect() {
+    window.location.href = "http://127.0.0.1:5500/www/cloudblesk.site/error_pages/500.html";
+}
+
+function redirect_replace() {
+    window.location.replace("http://127.0.0.1:5500/www/cloudblesk.site/error_pages/500.html");
+}
