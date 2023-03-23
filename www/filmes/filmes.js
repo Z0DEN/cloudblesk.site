484d1da968e4e357078a7b0809300a9e4761331e
Z0DEN <maksarestov2000@gmail.com>
2023-03-24 03:36:01 +0700

    done

---
+++ www/filmes/filmes.js
@@ -0,0 +1,18 @@
+function action_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%A0%D1%8D%D0%BC%D0%B1%D0%BE:_%D0%9F%D0%B5%D1%80%D0%B2%D0%B0%D1%8F_%D0%BA%D1%80%D0%BE%D0%B2%D1%8C";
+}
+function mult_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%AE%D0%B6%D0%BD%D1%8B%D0%B9_%D0%9F%D0%B0%D1%80%D0%BA";
+}
+function fiction_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80_%D0%9A%D1%82%D0%BE";
+}
+function a1_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80_%D0%9A%D1%82%D0%BE";
+}
+function a2_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80_%D0%9A%D1%82%D0%BE";
+}
+function a3_redirect() {
+    window.location.href = "https://ru.wikipedia.org/wiki/%D0%94%D0%BE%D0%BA%D1%82%D0%BE%D1%80_%D0%9A%D1%82%D0%BE";
+}
