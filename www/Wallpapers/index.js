a64af4749b0efc65a4168ac10856e88329aae0c1
Z0DEN <maksarestov2000@gmail.com>
2023-03-28 00:46:11 +0700

    experiments

--- www/Wallpapers/index.js
+++ www/Wallpapers/index.js
@@ -1,3 +1,11 @@
-function RndImg(){
-    
+function getRandomInt(max) {
+        console.log(Math.floor(Math.random() * max))
 }
+console.log(getRandomInt(24))
+
+
+var list = document.getElementById('hello') 
+var h1 = document.createElement('h1')
+h1.innerHTML = `${document.getElementById('header11').innerHTML}`
+h1.id = ('newH1')
+list.appendChild(h1)
