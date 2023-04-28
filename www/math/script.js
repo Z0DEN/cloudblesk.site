7dd7e004258e764671378178377d1f45fe1355e3
Z0DEN <maksarestov2000@gmail.com>
2023-04-29 00:16:28 +0700

    done

--- www/math/script.js
+++ www/math/script.js
@@ -18,12 +18,14 @@ function rsp(UserChoice) {      //Фунция на основе рандомн�
     }
     if (UserChoice === R){        //Если пользоваетель выбрал камень
       alert (`Победа : ${UserChoice} VS ${S}`)
+      user_score += 1000
     }
   }
 
   else if (rndInt === 1){    //Если машина выбрала камень
     if (UserChoice === P){        //Если пользоваетель выбрал бумагу
       alert (`Победа : ${UserChoice} VS ${R}`)
+      user_score += 1000
     }
     if (UserChoice === S){        //Если пользоваетель выбрал ножницы
       alert (`Поражение : ${UserChoice} VS ${R}`)
@@ -39,6 +41,7 @@ function rsp(UserChoice) {      //Фунция на основе рандомн�
     }
     if (UserChoice === S){      //Если пользоваетель выбрал ножницы
       alert (`Победа : ${UserChoice} VS ${P}`)
+      user_score += 1000
     }
     if (UserChoice === R){      //Если пользоваетель выбрал камень
       alert (`Поражение : ${UserChoice} VS ${P}`)
@@ -47,6 +50,7 @@ function rsp(UserChoice) {      //Фунция на основе рандомн�
   trp_div.backgroundColor = "rgba(255, 255, 255, 0)"
   trp_div.zIndex = "-999"
   alert('its iiiiiif brooooooo')
+  span_balance.innerHTML = `${user_score}`
 }
 
 // Следующая функция абсолютно схожа с предыдущей, но написана с использованием кнструкции [switch]
@@ -60,7 +64,8 @@ function rsp_switch(UserChoice) {      //Фунция на основе ранд
         break;
         case S: alert (`Ничья : ${UserChoice} VS ${S}`)
         break;
-        case R: alert (`Победа : ${UserChoice} VS ${S}`)
+        case R: alert (`Победа : ${UserChoice} VS ${S}`);
+        user_score += 1000;
         break;
       }
     break;
@@ -69,6 +74,7 @@ function rsp_switch(UserChoice) {      //Фунция на основе ранд
         case P: alert (`Ничья : ${UserChoice} VS ${P}`)
         break;
         case S: alert (`Победа : ${UserChoice} VS ${P}`)
+        user_score += 1000;
         break;
         case R: alert (`Поражение : ${UserChoice} VS ${P}`)
         break;
@@ -76,7 +82,8 @@ function rsp_switch(UserChoice) {      //Фунция на основе ранд
     break;
     case 2:
       switch (UserChoice){       //Rock
-        case P: alert (`Победа : ${UserChoice} VS ${R}`)
+        case P: alert (`Победа : ${UserChoice} VS ${R}`);
+        user_score += 1000;
         break;
         case S: alert (`Поражение : ${UserChoice} VS ${R}`)
         break;
@@ -88,7 +95,10 @@ function rsp_switch(UserChoice) {      //Фунция на основе ранд
   trp_div.backgroundColor = "rgba(255, 255, 255, 0)"
   trp_div.zIndex = "-999"
   alert('its a swiiiiitch brooooooo')
+  span_balance.innerHTML = `${user_score}`
 }
+
+const trp_div = document.getElementById("transparent_div").style;           //Получаем 
 
 function dialog_construction_choice(){      //Показываем диалоговое окно выбора конструкции
   trp_div.backgroundColor = "rgba(161, 113, 113, 0.493)"
@@ -102,22 +112,6 @@ function choice_switch(){
 
 function choice_if(){
   window.myDialog_choice_if.show()      //Если пользователь выбрал [if]
-}
-
-const trp_div = document.getElementById("transparent_div").style;           //Получаем стили дива с диалоговым окном
-
-// Для кмн на [switch]
-function dialog_user_choice_switch(){
-  trp_div.backgroundColor = "rgba(161, 113, 113, 0.493)"      //Добавляем розовый фон
-  trp_div.zIndex = "999"      //поднимаем диалоговое окно наверх, чтобы работало
-  window.myDialog.show()      //Показываем диалоговое окно
-}
-
-// Для кмн на [if]
-function dialog_user_choice_if(){
-  trp_div.backgroundColor = "rgba(161, 113, 113, 0.493)"      //Добавляем розовый фон
-  trp_div.zIndex = "999"      //поднимаем диалоговое окно наверх, чтобы работало
-  window.myDialog.show()      //Показываем диалоговое окно
 }
 
 
@@ -141,12 +135,22 @@ function calc(){      // Функция калькулятора
     case "/" : 
     result = value_1 / value_2
     alert (`${value_1} / ${value_2} = ${result}`)
+    if (value_2 === '0'){
+      alert("НЕЛЬЗЯ ДЕЛИТЬ НА НОЛЬ")
+    }
     break;
     case "**" : 
     result = value_1**value_2
     alert (`${value_1} ** ${value_2} = ${result}`)
     break;
   }
+  if (result === Infinity){
+    user_score += -1000
+  }
+  else {
+    user_score += 100
+  }
+  span_balance.innerHTML = `${user_score}`
 }
 
 function testRandomizer(){      //Функция рандомайзер. Благодаря ей значения чисел в тесте могут совпадать с вероятностью 1 к 3.
@@ -174,33 +178,84 @@ var input_1 = document.getElementsByClassName("test-1-input")
 var input_2 = document.getElementsByClassName("test-2-input")
 testRandomizer()
 
-function test(){
+function test(sign){
   num1 = document.getElementsByClassName('test-1-input')[0].innerHTML     //значение первого [span]
   num2 = document.getElementsByClassName('test-2-input')[0].innerHTML     //значение второго [span]
-  let sign = document.getElementById("select-test").value     //значение знака [select]
   switch(sign){
     case ">" : test_check = (num1 > num2) && (sign === ">") ? true : false; break;     //проверка
     case "<" : test_check = (num1 < num2) && (sign === "<") ? true : false; break;     //проверка
     case "=" : test_check = (num1 === num2) && (sign === "=") ? true : false; break;   //проверка
+    case "choice" : break;
   }
 
-  const form = document.getElementById('test').style;                 //Получаем стили формы
-  const img_checkbox = document.getElementById('checkbox').style;     //Получаем стили картинки галочки
-  const img_cross = document.getElementById('cross').style;           //Получаем стили картинки крестика
-
-  if (document.getElementById('fix_div').innerHTML != 'its a crutch'){      //Проверяем есть ли в невидимом элементе текст. Если да значит функция уже выполняеться
-    if (test_check){      //Если пользователь ответил правильно
-      img_checkbox.animation = 'appearance 3000ms ease-in-out';         //Запускаем анимацию появления для галочки.      Возможно костыль но по-другому не знаю как
-      form.animation = 'appearance_checkbox_back 3000ms ease-in-out'    //Запускаем анимацию появления зеленого фона.    Возможно костыль но по-другому не знаю как
-      document.getElementById('fix_div').innerHTML = 'its a crutch'     //вставляем в невидимый элемент текст 
-    }
-    else {
+  
+  if (test_check){      //Если пользователь ответил правильно
+    const form = document.getElementById('test').style;                 //Получаем стили формы
+    const img_checkbox = document.getElementById('checkbox').style;     //Получаем стили картинки галочки
+    img_checkbox.animation = 'appearance 3000ms ease-in-out';         //Запускаем анимацию появления для галочки.      Возможно костыль но по-другому не знаю как
+    form.animation = 'appearance_checkbox_back 3000ms ease-in-out'    //Запускаем анимацию появления зеленого фона.    Возможно костыль но по-другому не знаю как
+    user_score += 500
+    span_balance.innerHTML = `${user_score}`
+  }
+  else {
+      const form = document.getElementById('test').style;                 //Получаем стили формы
+      const img_cross = document.getElementById('cross').style;           //Получаем стили картинки крестика
       img_cross.animation = 'appearance 3000ms ease-in-out';            //Иначе запускаем анимацию появления для крестика
       form.animation = 'appearance_cross_back 3000ms ease-in-out'       //и анимацию появления красного фона
-      document.getElementById('fix_div').innerHTML = 'its a crutch'     //вставляем в невидимый элемент текст 
+      user_score += -500
+      span_balance.innerHTML = `${user_score}`
     }
     setTimeout(() => {      //Обновляем страницу для получения новых чисел
-      window.location.reload();
+      const form = document.getElementById('test').style;                 //Получаем стили формы
+      const img_cross = document.getElementById('cross').style;
+      const img_checkbox = document.getElementById('checkbox').style
+      form.animation = '1'
+      img_cross.animation = '1'
+      img_checkbox.animation = '1'
+      testRandomizer();
     }, 3100);
   }
+
+
+// 3 задание [WHILE], [FOR]🔽🔽🔽
+// 3 задание [WHILE], [FOR]🔽🔽🔽
+// 3 задание [WHILE], [FOR]🔽🔽🔽
+
+window.user_score = 0
+
+// while (user_score === 0){
+//   span_bal = document.getElementById('balance')
+//   span_bal.innerHTML = `${user_score}`
+// }
+
+
+// while (user_score < 2500){
+  
+// }
+
+function rules_dialog(){
+  trp_div.backgroundColor = "rgba(161, 113, 113, 0.493)"      //Добавляем розовый фон
+  trp_div.zIndex = "999"      //поднимаем диалоговое окно наверх, чтобы работало
+  window.show_rules.show()      //Показываем диалоговое окно
+}
+
+function reset(){
+  trp_div.backgroundColor = "rgba(255, 255, 255, 0)"
+  trp_div.zIndex = "-999"
+  window.show_rules.close()
+}
+
+
+span_balance = document.getElementById('balance')
+span_balance.innerHTML = `${user_score}`
+
+
+
+// do {
+//   span_bal.innerHTML = `${user_score}`
+// } while (user_score < 2500);
+
+
+if (user_score >= 2500){
+  alert("Поздравляем!!! Вы победили")
 }
