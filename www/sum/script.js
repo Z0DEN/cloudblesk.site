e5a62e4766114a307d88326d6bf4cea3043d5a5f
Z0DEN <maksarestov2000@gmail.com>
2023-05-07 03:21:16 +0700

    fix

--- www/sum/script.js
+++ www/sum/script.js
@@ -3,7 +3,7 @@ function sum() {
 	var num2 = document.getElementById("num2").value;
 
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "/api/sum?num1=" + num1 + "&num2=" + num2, true);
+	xhr.open("GET", "/api/cloud_api/sum?num1=" + num1 + "&num2=" + num2, true);
 	xhr.onload = function() {
 		document.getElementById("result").innerHTML = xhr.responseText;
 	};
