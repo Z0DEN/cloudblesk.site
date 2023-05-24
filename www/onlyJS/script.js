295c6342cbdd4c0401181e22c374f0bbc1f6d763
root <root@main>
2023-05-24 16:36:58 +0300

    Revert "done"
    This reverts commit dd754f884ac2a6623ac606f416f762eaedc3f8d5.

--- www/onlyJS/script.js
+++ www/onlyJS/script.js
@@ -1,6 +1,3 @@
-//фавиконка квадрата
-favicon = document.getElementById('favicon').setAttribute('href', 'https://img.icons8.com/?size=512&id=EeuWKT8PLduA&format=png')
-
 const body = document.querySelector('body');
 //создаем контейнер [grid]
 const container = document.createElement('div');
@@ -63,15 +60,13 @@ block.addEventListener('click', () => {
   // Меняем форму блока на круг или квадрат в зависимости от его текущей формы
   if (isRound) {
     isRound = false;
-    block.classList.remove('round');                                    //фавиконка квадрата
-    favicon = document.getElementById('favicon').setAttribute('href', 'https://img.icons8.com/?size=512&id=EeuWKT8PLduA&format=png')
+    block.classList.remove('round');
     for (let i = 0; i < grid_item.length; i++) {
       grid_item[i].classList.remove('round');
     };
   } else {
     isRound = true;
-    block.classList.add('round');                                       //фавиконка круга
-    favicon = document.getElementById('favicon').setAttribute('href', 'https://img.icons8.com/?size=512&id=N-aPJN6VfRBa&format=png')
+    block.classList.add('round');
     for (let i = 0; i < grid_item.length; i++) {
       grid_item[i].classList.add('round') ;
     };
