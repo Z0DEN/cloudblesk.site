7cde4aca97bee40d3a305d02ff80d1048cc7d2e5
BlesK <maksarestov2000@gmail.com>
2023-05-25 20:59:25 +0300

    test

--- www/sum/cloud_api/views.py
+++ www/sum/cloud_api/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 def sum(request):
 	num1 = int(request.GET.get("num1"))
 	num2 = int(request.GET.get("num2"))
-	result = num1 + num2
+	result = num1 * num2
 	return HttpResponse(result)
