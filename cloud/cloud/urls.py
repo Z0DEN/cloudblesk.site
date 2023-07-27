369eeb02a9e2a9321049e75c413f2b90176869a4
root <maksarestov2000@gmail.com>
2023-07-27 18:50:13 +0300

    27.07.23

--- cloud/cloud/urls.py
+++ cloud/cloud/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('accounts', include("django.contrib.auth.urls")),
+    path('accounts/', include("django.contrib.auth.urls")),
     path('registration/', include('registration.urls')),
     path('cloud_api/', include('cloud_api.urls')),
     path('db/', include('db.urls')),
