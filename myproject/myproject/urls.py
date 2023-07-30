181a8e8a22f484cb19ce4d690baa71bdd0b682ee
root <maksarestov2000@gmail.com>
2023-07-30 17:41:46 +0300

    first commit

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cloud project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,15 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 
 urlpatterns = [
-    path('accounts', include("django.contrib.auth.urls")),
-    path('registration/', include('registration.urls')),
-    path('cloud_api/', include('cloud_api.urls')),
-    path('db/', include('db.urls')),
     path('admin/', admin.site.urls),
 ]
